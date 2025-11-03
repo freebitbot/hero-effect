5e9ce2c06e1d6e647b22e7c55849facdcd382779
Vasyl Sokolyk <vsokolyk@gmail.com>
2025-11-03 23:30:16 +0200

    git mv double-agent-plugins

--- apps/double-agent/analyze/lib/getAllPlugins.ts
+++ apps/double-agent/analyze/lib/getAllPlugins.ts
@@ -1,36 +1,39 @@
-import * as Fs from 'fs';
-import * as Path from 'path';
-import Plugin from './Plugin';
+import * as Fs from "node:fs";
+import * as Path from "node:path";
+import type Plugin from "./Plugin";
 
-export const pluginsDir = Path.resolve(__dirname, '../plugins');
+export const pluginsDir = Path.resolve(__dirname, "../plugins");
 
-export default function getAllPlugins(print = false, filter?: string[]): Plugin[] {
-  const plugins: Plugin[] = [];
+export default function getAllPlugins(
+	print = false,
+	filter?: string[],
+): Plugin[] {
+	const plugins: Plugin[] = [];
 
-  for (const pluginDirName of Fs.readdirSync(pluginsDir)) {
-    const pluginDir = Path.join(pluginsDir, pluginDirName);
-    if (pluginDirName === '.DS_Store') continue;
-    if (!Fs.statSync(pluginDir).isDirectory()) continue;
-    if (filter && !filter.includes(pluginDirName)) continue;
-    // if (pluginDirName !== 'browser-fingerprints') continue;
+	for (const pluginDirName of Fs.readdirSync(pluginsDir)) {
+		const pluginDir = Path.join(pluginsDir, pluginDirName);
+		if (pluginDirName === ".DS_Store") continue;
+		if (!Fs.statSync(pluginDir).isDirectory()) continue;
+		if (filter && !filter.includes(pluginDirName)) continue;
+		// if (pluginDirName !== 'browser-fingerprints') continue;
 
-    try {
-      // eslint-disable-next-line global-require,import/no-dynamic-require
-      const AnalyzePlugin = require(pluginDir)?.default;
-      if (AnalyzePlugin) {
-        plugins.push(new AnalyzePlugin(pluginDir));
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }
+		try {
+			// eslint-disable-next-line global-require,import/no-dynamic-require
+			const AnalyzePlugin = require(pluginDir)?.default;
+			if (AnalyzePlugin) {
+				plugins.push(new AnalyzePlugin(pluginDir));
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
-  if (print) {
-    console.log(
-      'Analyze Plugins Activated',
-      plugins.map(x => `${x ? '✓' : 'x'} ${x.id} - ${x.summary}`),
-    );
-  }
+	if (print) {
+		console.log(
+			"Analyze Plugins Activated",
+			plugins.map((x) => `${x ? "✓" : "x"} ${x.id} - ${x.summary}`),
+		);
+	}
 
-  return plugins;
+	return plugins;
 }
