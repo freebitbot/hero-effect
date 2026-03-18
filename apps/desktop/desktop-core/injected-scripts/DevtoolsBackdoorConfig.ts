dd67e2a5b1f6a825b398bc026ee25b1134f31ec8
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-18 23:48:24 +0200

    remove @ulixee/desktop-core

--- apps/desktop/desktop-core/injected-scripts/DevtoolsBackdoorConfig.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export const ___emitFromDevtoolsToCore = "___emitFromDevtoolsToCore";
-
-export const EventType = {
-	ElementWasSelected: "ElementWasSelected",
-	ToggleInspectElementMode: "ToggleInspectElementMode",
-	DevtoolsFocus: "DevtoolsFocus",
-};
