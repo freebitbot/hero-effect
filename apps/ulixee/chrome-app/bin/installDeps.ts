c9bdd41a0e30a1796ea16d6afe0037c0bf77ea3a
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-18 16:40:37 +0200

    biome check --fix apps/ulixee/chrome-app

--- apps/ulixee/chrome-app/bin/installDeps.ts
+++ apps/ulixee/chrome-app/bin/installDeps.ts
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 
-import * as Fs from 'fs';
-import * as Path from 'path';
-import { isDebianFlavor } from '../lib/LinuxUtils';
-import ChromeApp from '../index';
+import * as Fs from "fs";
+import * as Path from "path";
+import ChromeApp from "../index";
+import { isDebianFlavor } from "../lib/LinuxUtils";
 
 function noOp() {
-  process.stdout.write(Path.join(__dirname, 'no-op.sh'));
+	process.stdout.write(Path.join(__dirname, "no-op.sh"));
 }
 
-if (process.platform === 'linux') {
-  (async () => {
-    const isDebian = await isDebianFlavor();
-    if (isDebian) {
-      const path = ChromeApp.aptScriptPath;
-      if (Fs.existsSync(path)) {
-        process.stdout.write(path);
-        return;
-      }
-    }
-    noOp();
-  })().catch(() => null);
+if (process.platform === "linux") {
+	(async () => {
+		const isDebian = await isDebianFlavor();
+		if (isDebian) {
+			const path = ChromeApp.aptScriptPath;
+			if (Fs.existsSync(path)) {
+				process.stdout.write(path);
+				return;
+			}
+		}
+		noOp();
+	})().catch(() => null);
 } else {
-  noOp();
+	noOp();
 }
