c77005f3a926ef026d60f42b1207c17fad7a88f0
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-18 16:54:43 +0200

    biome check --fix apps/ulixee/unblocked-browser-emulator-builder

--- apps/ulixee/unblocked-browser-emulator-builder/lib/EmulatorData.ts
+++ apps/ulixee/unblocked-browser-emulator-builder/lib/EmulatorData.ts
@@ -1,20 +1,23 @@
-import { emulatorDataDir } from '../paths';
+import { emulatorDataDir } from "../paths";
 
 export default class EmulatorData {
-  public static get emulatorsDirPath(): string {
-    return emulatorDataDir;
-  }
+	public static get emulatorsDirPath(): string {
+		return emulatorDataDir;
+	}
 
-  public static getEmulatorDir(browserId: string): string {
-    const emulatorId = this.extractBrowserEngineId(browserId);
-    return `${this.emulatorsDirPath}/as-${emulatorId}`;
-  }
+	public static getEmulatorDir(browserId: string): string {
+		const emulatorId = EmulatorData.extractBrowserEngineId(browserId);
+		return `${EmulatorData.emulatorsDirPath}/as-${emulatorId}`;
+	}
 
-  public static getEmulatorDataOsDir(baseDataDir: string, operatingSystemId: string): string {
-    return `${baseDataDir}/as-${operatingSystemId}`;
-  }
+	public static getEmulatorDataOsDir(
+		baseDataDir: string,
+		operatingSystemId: string,
+	): string {
+		return `${baseDataDir}/as-${operatingSystemId}`;
+	}
 
-  public static extractBrowserEngineId(browserId: string): string {
-    return browserId.replace('chromium', 'chrome');
-  }
+	public static extractBrowserEngineId(browserId: string): string {
+		return browserId.replace("chromium", "chrome");
+	}
 }
