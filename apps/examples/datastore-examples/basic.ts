3b9881e42d83721d1860337cecf57c872a49d907
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-18 22:44:07 +0200

    remove examples

--- apps/examples/datastore-examples/basic.ts
+++
@@ -1,7 +0,0 @@
-// NOTE: you must start your own Ulixee Cloud to run this example.
-
-import { Extractor, HeroExtractorPlugin } from '@ulixee/datastore-plugins-hero';
-
-export default new Extractor(datastore => {
-  console.log('INPUT: ', datastore.input);
-}, HeroExtractorPlugin);
