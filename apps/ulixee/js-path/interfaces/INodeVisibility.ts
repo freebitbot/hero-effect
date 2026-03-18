9f81fdeea86ce88a88cd041d184ea250cc0d2880
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-18 16:51:15 +0200

    biome check --fix apps/ulixee/js-path

--- apps/ulixee/js-path/interfaces/INodeVisibility.ts
+++ apps/ulixee/js-path/interfaces/INodeVisibility.ts
@@ -1,19 +1,19 @@
-import IElementRect from './IElementRect';
+import type IElementRect from "./IElementRect";
 
 export default interface INodeVisibility {
-  isVisible: boolean;
-  isClickable: boolean;
-  nodeExists?: boolean;
-  isOnscreenVertical?: boolean;
-  isOnscreenHorizontal?: boolean;
-  hasContainingElement?: boolean;
-  isConnected?: boolean;
-  hasCssOpacity?: boolean;
-  hasCssDisplay?: boolean;
-  hasCssVisibility?: boolean;
-  hasDimensions?: boolean;
-  obstructedByElementId?: number;
-  obstructedByElementRect?: IElementRect;
-  isUnobstructedByOtherElements?: boolean;
-  boundingClientRect?: IElementRect;
+	isVisible: boolean;
+	isClickable: boolean;
+	nodeExists?: boolean;
+	isOnscreenVertical?: boolean;
+	isOnscreenHorizontal?: boolean;
+	hasContainingElement?: boolean;
+	isConnected?: boolean;
+	hasCssOpacity?: boolean;
+	hasCssDisplay?: boolean;
+	hasCssVisibility?: boolean;
+	hasDimensions?: boolean;
+	obstructedByElementId?: number;
+	obstructedByElementRect?: IElementRect;
+	isUnobstructedByOtherElements?: boolean;
+	boundingClientRect?: IElementRect;
 }
