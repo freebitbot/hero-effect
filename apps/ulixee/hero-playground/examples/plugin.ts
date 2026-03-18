35a8da8ebe7691c0a01c0e6f0006acdce5fcfa72
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-18 16:50:18 +0200

    biome check --fix apps/ulixee/hero-playground

--- apps/ulixee/hero-playground/examples/plugin.ts
+++ apps/ulixee/hero-playground/examples/plugin.ts
@@ -1,46 +1,45 @@
-import DefaultBrowserEmulator from '@ulixee/default-browser-emulator';
-import { ConnectionToHeroCore } from '@ulixee/hero';
-import HeroCore from '@ulixee/hero-core';
-import ExecuteJsPlugin from '@ulixee/execute-js-plugin';
-import Hero, { LocationTrigger } from '@ulixee/hero';
-import TransportBridge from '@ulixee/net/lib/TransportBridge';
+import DefaultBrowserEmulator from "@ulixee/default-browser-emulator";
+import ExecuteJsPlugin from "@ulixee/execute-js-plugin";
+import Hero, { ConnectionToHeroCore, LocationTrigger } from "@ulixee/hero";
+import HeroCore from "@ulixee/hero-core";
+import TransportBridge from "@ulixee/net/lib/TransportBridge";
 
 async function run() {
-  HeroCore.defaultUnblockedPlugins = [DefaultBrowserEmulator];
-  HeroCore.use(ExecuteJsPlugin);
-  const bridge = new TransportBridge();
-  const connectionToCore = new ConnectionToHeroCore(bridge.transportToCore);
+	HeroCore.defaultUnblockedPlugins = [DefaultBrowserEmulator];
+	HeroCore.use(ExecuteJsPlugin);
+	const bridge = new TransportBridge();
+	const connectionToCore = new ConnectionToHeroCore(bridge.transportToCore);
 
-  const heroCore = new HeroCore();
-  heroCore.addConnection(bridge.transportToClient);
+	const heroCore = new HeroCore();
+	heroCore.addConnection(bridge.transportToClient);
 
-  const hero = new Hero({ connectionToCore });
-  await hero.goto('https://example.org/');
-  await hero.waitForPaintingStable();
+	const hero = new Hero({ connectionToCore });
+	await hero.goto("https://example.org/");
+	await hero.waitForPaintingStable();
 
-  console.log('\n-- PRINTING location.href ---------');
-  console.log(await hero.url);
+	console.log("\n-- PRINTING location.href ---------");
+	console.log(await hero.url);
 
-  const outerHtml = await hero.document.documentElement.outerHTML;
-  const linkElement = hero.document.querySelector('a');
+	const outerHtml = await hero.document.documentElement.outerHTML;
+	const linkElement = hero.document.querySelector("a");
 
-  console.log('-- PRINTING outerHTML of link ---------------');
-  console.log(outerHtml);
-  console.log('OUTPUT from https://example.org', {
-    outerHtml,
-    title: await hero.document.title,
-    intro: await hero.document.querySelector('p').textContent,
-    linkTag: await linkElement.outerHTML,
-  });
-  console.log('-------------------------------------');
+	console.log("-- PRINTING outerHTML of link ---------------");
+	console.log(outerHtml);
+	console.log("OUTPUT from https://example.org", {
+		outerHtml,
+		title: await hero.document.title,
+		intro: await hero.document.querySelector("p").textContent,
+		linkTag: await linkElement.outerHTML,
+	});
+	console.log("-------------------------------------");
 
-  await linkElement.$click();
-  await hero.waitForLocation(LocationTrigger.change);
-  console.log('NEW LOCATION: ', await hero.document.location.href);
-  console.log('-------------------------------------');
+	await linkElement.$click();
+	await hero.waitForLocation(LocationTrigger.change);
+	console.log("NEW LOCATION: ", await hero.document.location.href);
+	console.log("-------------------------------------");
 
-  await hero.close();
-  await heroCore.close();
+	await hero.close();
+	await heroCore.close();
 }
 
-run().catch(error => console.log(error));
+run().catch((error) => console.log(error));
--- apps/ulixee/hero-playground/index.ts
+++ apps/ulixee/hero-playground/index.ts
@@ -1,40 +1,47 @@
-import '@ulixee/commons/lib/SourceMapSupport';
-import DefaultHero, { ConnectionToHeroCore, IHeroCreateOptions } from '@ulixee/hero';
-import Core from '@ulixee/hero-core';
-import TransportBridge from '@ulixee/net/lib/TransportBridge';
+import "@ulixee/commons/lib/SourceMapSupport";
+import DefaultHero, {
+	ConnectionToHeroCore,
+	type IHeroCreateOptions,
+} from "@ulixee/hero";
+import Core from "@ulixee/hero-core";
+import TransportBridge from "@ulixee/net/lib/TransportBridge";
 
-export * from '@ulixee/hero';
+export * from "@ulixee/hero";
 export { Core };
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 let _heroCore: Core;
 function initCore(): Core {
-  if (_heroCore) return _heroCore;
-  Core.events.once('browser-has-no-open-windows', ({ browser }) => browser.close());
-  Core.events.once('all-browsers-closed', () => {
-    // eslint-disable-next-line no-console
-    console.log('Automatically shutting down Hero Core (Browser Closed)');
-    return Core.shutdown();
-  });
-  _heroCore = new Core();
-  return _heroCore;
+	if (_heroCore) return _heroCore;
+	Core.events.once("browser-has-no-open-windows", ({ browser }) =>
+		browser.close(),
+	);
+	Core.events.once("all-browsers-closed", () => {
+		// eslint-disable-next-line no-console
+		console.log("Automatically shutting down Hero Core (Browser Closed)");
+		return Core.shutdown();
+	});
+	_heroCore = new Core();
+	return _heroCore;
 }
 
 let counter = 0;
 export default class Hero extends DefaultHero {
-  constructor(createOptions: IHeroCreateOptions = {}) {
-    counter += 1;
-    if (counter > 1) {
-      console.warn(`You've launched multiple instances of Hero using Hero Playgrounds. @ulixee/hero-playgrounds is intended to help you get started with examples, but will try to automatically shut down after the first example is run. 
+	constructor(createOptions: IHeroCreateOptions = {}) {
+		counter += 1;
+		if (counter > 1) {
+			console.warn(`You've launched multiple instances of Hero using Hero Playgrounds. @ulixee/hero-playgrounds is intended to help you get started with examples, but will try to automatically shut down after the first example is run. 
       
 If you're starting to run real production scenarios, you likely want to look into converting to a Client/Core setup: 
 
 https://ulixee.org/docs/hero/advanced-concepts/client-vs-core
 `);
-    }
-    const transportBridge = new TransportBridge();
-    createOptions.connectionToCore = new ConnectionToHeroCore(transportBridge.transportToCore);
-    initCore().addConnection(transportBridge.transportToClient);
-    super(createOptions);
-  }
+		}
+		const transportBridge = new TransportBridge();
+		createOptions.connectionToCore = new ConnectionToHeroCore(
+			transportBridge.transportToCore,
+		);
+		initCore().addConnection(transportBridge.transportToClient);
+		super(createOptions);
+	}
 }
