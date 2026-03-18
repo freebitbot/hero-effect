3ce6d628976a866e24291d69c45ec0e654b26f49
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-18 16:48:09 +0200

    biome check --fix apps/ulixee/chrome-version-template

--- apps/ulixee/chrome-version-template/index.ts
+++ apps/ulixee/chrome-version-template/index.ts
@@ -1,14 +1,15 @@
-import ChromeApp from '@ulixee/chrome-app';
-const { fullVersion, fullVersionOverridesByOs } = require('./package.json');
+import ChromeApp from "@ulixee/chrome-app";
+
+const { fullVersion, fullVersionOverridesByOs } = require("./package.json");
 
 class Chrome extends ChromeApp {
-  constructor(options?: {
-    executablePathEnvVar?: string;
-    osPlatformName?: ChromeApp['osPlatformName'];
-  }) {
-    options ??= {};
-    super(fullVersion, { ...options, fullVersionOverridesByOs });
-  }
+	constructor(options?: {
+		executablePathEnvVar?: string;
+		osPlatformName?: ChromeApp["osPlatformName"];
+	}) {
+		options ??= {};
+		super(fullVersion, { ...options, fullVersionOverridesByOs });
+	}
 }
 
 export = Chrome;
