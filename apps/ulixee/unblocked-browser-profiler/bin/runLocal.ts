a1b90cf91e55eccbca004f2693e347d92b7c94db
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-18 16:54:59 +0200

    biome check --fix apps/ulixee/unblocked-browser-profiler

--- apps/ulixee/unblocked-browser-profiler/bin/runLocal.ts
+++ apps/ulixee/unblocked-browser-profiler/bin/runLocal.ts
@@ -10,33 +10,33 @@
  * add an entry
  * 1 6 * * * cd $HOME/Projects/ulixee/hero/browser-profiler/build/ && npx runLocal
  */
-import '@ulixee/commons/lib/SourceMapSupport';
-import { spawnSync } from 'child_process';
-import runLocalDoms from '../scripts/runLocalDoms';
-import { profileDataDir } from '../paths';
+import "@ulixee/commons/lib/SourceMapSupport";
+import { spawnSync } from "child_process";
+import { profileDataDir } from "../paths";
+import runLocalDoms from "../scripts/runLocalDoms";
 
 (async function runLocal(): Promise<void> {
-  console.log('Pulling latest from Profile Data');
-  spawnSync('git pull', {
-    cwd: profileDataDir,
-    stdio: ['inherit', 'inherit', 'inherit'],
-    encoding: 'utf8',
-    shell: true,
-  });
-  await runLocalDoms(false, true).catch(console.error);
+	console.log("Pulling latest from Profile Data");
+	spawnSync("git pull", {
+		cwd: profileDataDir,
+		stdio: ["inherit", "inherit", "inherit"],
+		encoding: "utf8",
+		shell: true,
+	});
+	await runLocalDoms(false, true).catch(console.error);
 
-  console.log('Adding all new files');
-  spawnSync(`git add . && git commit -m 'chore: run local doms on mac'`, {
-    cwd: profileDataDir,
-    stdio: ['inherit', 'inherit', 'inherit'],
-    encoding: 'utf8',
-    shell: true,
-  });
-  console.log('Pushing to main');
-  spawnSync('git push origin main', {
-    cwd: profileDataDir,
-    stdio: ['inherit', 'inherit', 'inherit'],
-    encoding: 'utf8',
-    shell: true,
-  });
+	console.log("Adding all new files");
+	spawnSync(`git add . && git commit -m 'chore: run local doms on mac'`, {
+		cwd: profileDataDir,
+		stdio: ["inherit", "inherit", "inherit"],
+		encoding: "utf8",
+		shell: true,
+	});
+	console.log("Pushing to main");
+	spawnSync("git push origin main", {
+		cwd: profileDataDir,
+		stdio: ["inherit", "inherit", "inherit"],
+		encoding: "utf8",
+		shell: true,
+	});
 })().catch(console.error);
