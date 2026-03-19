6dad446b27474714287c00b9b972c57d73ec435b
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-19 12:57:17 +0200

    Remove node registry cloud components

--- apps/platform/cloud/interfaces/ICloudConfiguration.ts
+++ apps/platform/cloud/interfaces/ICloudConfiguration.ts
@@ -1,14 +1,8 @@
 import type { ServerOptions } from "node:https";
 import type { ListenOptions } from "node:net";
-import type Identity from "@ulixee/platform-utils/lib/Identity";
 
 export default interface ICloudConfiguration
 	extends ListenOptions,
 		ServerOptions {
-	networkIdentity?: Identity;
-	nodeRegistryHost: string | "self";
 	publicHost?: string;
-	hostedServicesServerOptions?: ListenOptions & ServerOptions;
-	servicesSetupHost: string;
-	disableDesktopCore?: boolean;
 }
