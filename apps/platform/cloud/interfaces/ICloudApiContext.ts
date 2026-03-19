4d73280e73a86764f6d5722a29e9f12043365bbd
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-19 12:57:17 +0200

    Remove node registry cloud components

--- apps/platform/cloud/interfaces/ICloudApiContext.ts
+++ apps/platform/cloud/interfaces/ICloudApiContext.ts
@@ -1,16 +1,9 @@
 import type { IBoundLog } from "@ulixee/commons/interfaces/ILog";
-import type IDatastoreCoreConfigureOptions from "@ulixee/datastore-core/interfaces/IDatastoreCoreConfigureOptions";
-import type NodeRegistry from "../lib/NodeRegistry";
-import type NodeTracker from "../lib/NodeTracker";
 import type ICloudConfiguration from "./ICloudConfiguration";
 
 export default interface ICloudApiContext {
 	logger: IBoundLog;
-	nodeTracker: NodeTracker;
-	nodeRegistry: NodeRegistry;
 	cloudConfiguration: ICloudConfiguration;
-	datastoreConfiguration: IDatastoreCoreConfigureOptions;
 	nodeAddress: URL;
-	hostedServicesAddress: URL;
 	version: string;
 }
